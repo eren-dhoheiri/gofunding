1d33b951604441fdef345b1edf947d261c0a0191
dhoheiri <dhoheiri@gmail.com>
2021-01-23 00:12:34 +0700

    set middleware authorization on api upload avatar

--- user/service.go
+++ user/service.go
@@ -11,6 +11,7 @@ type Service interface {
 	Login(input LoginInput) (User, error)
 	IsEmailAvailable(input CheckEmailInput) (bool, error)
 	SaveAvatar(ID int, fileLocation string) (User, error)
+	GetUsersByID (ID int) (User, error)
 }
 
 type service struct {
@@ -96,4 +97,17 @@ func (s *service ) SaveAvatar(ID int, fileLocation string) (User, error) {
 	}
 
 	return updatedUser, nil
+}
+
+func (s *service) GetUsersByID(ID int) (User, error) {
+	user, err := s.repository.FindByID(ID)
+	if err != nil {
+		return user, err
+	}
+
+	if user.ID == 0 {
+		return user, errors.New("User ID not found")
+	}
+
+	return user, nil	
 }
